d08e6c3d9c960b8513d232f4a5c74eb0554f6ac1
chavaxx <sgaytani@live.com>
2021-09-06 18:05:32 -0500

    initial commit milam app

---
+++ app/src/main/java/a01730311/tec/milam/MainActivity.kt
@@ -0,0 +1,25 @@
+package a01730311.tec.milam
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import androidx.appcompat.app.ActionBar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // hides action bar
+        val actionBar: ActionBar? = supportActionBar
+        actionBar?.hide()
+        // sets translucent status bar
+        this.window.apply {
+            clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            statusBarColor = Color.TRANSPARENT
+        }
+    }
+}
