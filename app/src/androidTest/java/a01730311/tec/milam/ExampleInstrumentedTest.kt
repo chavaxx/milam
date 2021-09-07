ab710d92980630fb4ae8ac6b45fb00e3f56e5ae1
chavaxx <sgaytani@live.com>
2021-09-07 16:45:00 -0500

    front end inicio de sesion screen finished, new shape resources added

--- app/src/androidTest/java/a01730311/tec/milam/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/a01730311/tec/milam/ExampleInstrumentedTest.kt
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith
 import org.junit.Assert.*
 
 /**
- * Instrumented test, which will execute on an Android device.
+ * Instrumented inicio_sesion.xml, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
@@ -17,7 +17,7 @@ import org.junit.Assert.*
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
-        // Context of the app under test.
+        // Context of the app under inicio_sesion.xml.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("a01730311.tec.milam", appContext.packageName)
     }
