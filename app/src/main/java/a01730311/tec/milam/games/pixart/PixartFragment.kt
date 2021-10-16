d7fed2cc077d9d503879c04eeff1e0da9370ea52
main <49618395+ZukiGA@users.noreply.github.com>
2021-10-16 17:01:45 -0500

    feat(pause): :sparkles: Introducing pause dialog
    Need some changes

--- app/src/main/java/a01730311/tec/milam/games/pixart/PixartFragment.kt
+++ app/src/main/java/a01730311/tec/milam/games/pixart/PixartFragment.kt
@@ -6,27 +6,40 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import a01730311.tec.milam.R
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.widget.ImageView
+import com.google.android.material.card.MaterialCardView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [PixartFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PixartFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private lateinit var pauseButton: FloatingActionButton
+    private lateinit var pauseDialog: Dialog
+    private lateinit var closeDialogButton: ImageView
+    private lateinit var retryButton: MaterialCardView
+    private lateinit var exitGameButton: MaterialCardView
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        pauseDialog = Dialog(requireContext())
+        pauseButton = view.findViewById(R.id.pauseButton)
+        pauseButton.setOnClickListener {
+            pauseDialog.setContentView(R.layout.pause_dialog)
+            pauseDialog.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+            pauseDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+            closeDialogButton = pauseDialog.findViewById(R.id.close_pause_dialog)
+            retryButton = pauseDialog.findViewById(R.id.retry_button)
+            exitGameButton = pauseDialog.findViewById(R.id.exit_game_button)
+            exitGameButton.setOnClickListener {
+                pauseDialog.hide()
+            }
+            pauseDialog.show()
         }
     }
 
@@ -38,23 +51,4 @@ class PixartFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_pixart, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PixartFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PixartFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
